fd00b52ca01e7cbd81bba69d16f1d06698a11b4a
Hanai <ihanai1991@gmail.com>
2020-12-13 01:26:57 +0800

    add login_page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,39 +1,12 @@
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:vvex/login_page.dart';
+import 'package:html/parser.dart' show parse;
+import 'package:dio/dio.dart';
+import 'package:dio_cookie_manager/dio_cookie_manager.dart';
+import 'package:cookie_jar/cookie_jar.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -47,11 +20,63 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LoginPageState extends State<LoginPage> {
+  String _once;
+  Uint8List _captchaImg;
+
+  @override
+  void initState() {
+    super.initState();
+    this._getSigninWebPageData();
+  }
+
+  Future<void> _getSigninWebPageData() async {
+    var dio = Dio();
+    var cookieJar = CookieJar();
+    dio.interceptors.add(CookieManager(cookieJar));
+
+    var url = 'https://www.v2ex.com/signin';
+
+    var r =
+        await dio.get(url, options: Options(responseType: ResponseType.plain));
+    final String body = r.data.toString();
+
+    var document = parse(body);
+    var loginForm = document.querySelector('form[method="post"]');
+
+    var once;
+    var inputOnce = loginForm.querySelector('input[name="once"]');
+    if (inputOnce != null) {
+      once = inputOnce.attributes['value'];
+    }
+
+    var captchaUrl;
+    var captchaDiv = loginForm.querySelector('div[style^="background-image"]');
+    if (captchaDiv != null) {
+      RegExp exp = new RegExp(r"url\('(.+)'\)");
+      final match = exp.firstMatch(captchaDiv.attributes['style']);
+      if (match != null &&
+          match.groupCount > 0 &&
+          match.group(1).startsWith('/')) {
+        captchaUrl = 'https://www.v2ex.com' + match.group(1);
+      }
+    }
+
+    var captchaImg;
+    if (captchaUrl != null) {
+      r = await dio.get(captchaUrl,
+          options: Options(responseType: ResponseType.bytes));
+      captchaImg = r.data;
+    }
+
+    setState(() {
+      _once = once;
+      _captchaImg = captchaImg;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,6 +86,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -90,23 +116,10 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            _captchaImg != null ? Image.memory(_captchaImg) : Placeholder()
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => LoginPage(title: '登录')),
-          );
-        },
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
