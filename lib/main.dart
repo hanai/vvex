ac1caac824ff52a9936bf3a4e01b497e2ad6a11c
Hanai <ihanai1991@gmail.com>
2021-01-09 17:00:56 +0800

    localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'providers/user_state.dart';
 import 'services/navigation_service.dart';
@@ -19,6 +22,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        RefreshLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale.fromSubtags(languageCode: 'zh'),
+        const Locale('en'),
+      ],
       navigatorKey: locator<NavigationService>().navigatorKey,
       title: 'VVEX',
       onGenerateRoute: router.generateRoute,
